e3794f6b601dbc730376f1932e0c25e5bfd27df8
이정현 <wjd1gus2@gmail.com>
2026-03-13 22:49:34 +0900

    머지 정렬, 이분탐색 수찾기, 색종이

--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,22 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+
+  
+def solution(x, y, n):
+  white = 0
+  blue = 0
+  n = 8
+  arr = []
+  x = 0
+  y = 0
+
+  for i in range(n):
+    for j in range(n):
+      if arr[i][j] != 1:
+        solution(x,y,n // 2)
+        solution(x, y + n//2, n // 2)
+        solution(x + n//2,y,n // 2)
+        solution(x + n//2,y + n//2, n // 2)
+      blue += 1
+      return
+  
--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,25 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+
+def solution():
+  target = 5
+  list_num = [1,3,7,9,8]
+  list_num.sort()
+  left = 0
+  right = len(list_num) - 1
+  mid = (left+right)//2
+
+  while left <= right:
+    if list_num[mid] == target:
+      return print("1")
+    if target < list_num[mid]:
+      right = mid - 1
+      mid = (left + right)//2
+    elif target > list_num[mid]:
+      left = mid + 1
+      mid = (left + right)//2
+  
+  print("0")
+
+
+solution()
