4378535a9726b03e9729a0bce0ceff93841a4e07
이정현 <wjd1gus2@gmail.com>
2026-03-17 23:04:55 +0900

    철도공사, 곱셈, 스택_탑

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,21 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+import sys
+
+def solution(a,b,c):
+  
+  if b == 1:
+    return a % c
+  
+  temp = solution(a, b // 2, c)
+
+  if b % 2 == 0:
+    return (temp * temp) % c
+  else:
+    return (temp * temp * a) % c
+  
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+print(solution(a,b,c))
--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,33 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+import sys
+
+def solution():
+  input_data = sys.stdin.read().split()
+  if not input_data:
+    return
+
+
+  n =  int(input_data[0])
+  list_num = list(map(int, input_data[1:]))
+  result = []
+  stack = []
+
+  for i in range(n):
+    current_top = list_num[i]
+    current_num = i + 1
+
+    while stack and stack[-1][0] < current_top:
+      stack.pop()
+    
+    if not stack:
+      result.append(0)
+    else:
+      result.append(stack[-1][1])
+    
+    stack.append((current_top, current_num))
+
+  print(*(result))
+
+solution()
