6673103ce57ba273ef55ac8be5c8f062ab821b41
이정현 <wjd1gus2@gmail.com>
2026-03-27 22:44:10 +0900

    DP메모이제이션, 상향식, 난이도 하

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,30 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+import sys
+
+def solution():
+  input = sys.stdin.readline
+    
+  n = int(input().strip())
+    
+  def fibo(num, memo = None):
+      if memo is None:
+        memo = {}
+
+      if num == 0:
+          return 0
+      if num == 1:
+          return 1
+
+      if num in memo:
+        return memo[num]
+
+      memo[num] = fibo(num - 1, memo) + fibo(num - 2, memo)
+
+      return memo[num]
+
+  print(fibo(n))
+
+if __name__ == "__main__":
+    solution()
