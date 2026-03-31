1a805997e2197745d58027232dda8b423064944f
이정현 <wjd1gus2@gmail.com>
2026-03-31 22:50:08 +0900

    난이도 중

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,31 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+
+
+
+
+def solution():
+  a_string = input()
+  b_string = input()
+
+  n = len(a_string)
+  m = len(b_string)
+
+  arr = [[0] * (m+1) for _ in range(n+1)]
+
+  for i in range(1, n+1):
+    for j in range(1, m+1):
+      first = a_string[i - 1]
+      second = b_string[j - 1]
+
+      if first == second:
+        arr[i][j] = arr[i-1][j-1] + 1
+      else:
+        arr[i][j] = max(arr[i][j-1], arr[i-1][j])
+
+  print(arr[n][m])
+
+solution()
+
+
+  
--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,35 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+import sys
+
+def solution():
+  T= int(sys.stdin.readline())
+
+  for _ in range(T):
+
+    N = int(sys.stdin.readline())
+
+    list_ = []
+
+    for _ in range(N):
+      p, i = map(int, sys.stdin.readline().split())
+      list_.append((p, i))
+
+    list_.sort(key=lambda x: x[0])
+
+    paper, interview = list_[0]
+
+    count = 1
+
+    for i in range(1, len(list_)):
+      current_paper, current_interview = list_[i]
+
+      if interview > current_interview:
+        count += 1
+        interview = current_interview
+
+    print(count)
+
+
+solution()
